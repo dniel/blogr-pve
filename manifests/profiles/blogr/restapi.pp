92351e5cc322ee08fea45930b6217824d82e5398
Daniel <daniel@engfeldt.net>
2016-06-15 21:21:57 +0200

    ordering and dependency of steps

--- manifests/profiles/blogr/restapi.pp
+++ manifests/profiles/blogr/restapi.pp
@@ -1,16 +1,20 @@
 class pve::profiles::blogr::restapi{
+  require nodejs
+
   vcsrepo { '/opt/blogr':
     ensure   => latest,
     provider => git,
     source   => 'https://github.com/dniel/blogr-workshop'
-  }
-
+  }->
   exec { 'npm install' :
-    cwd => '/opt/blogr',
+    cwd  => '/opt/blogr',
     user => 'root',
-    path => '/usr/local/node/node-default/bin'
-  }
+    path => '/usr/local/node/node-default/bin',
 
+  }->
+  file { '/etc/init.d/node-app':
+    source => 'puppet:///modules/pve/node-app'
+  }~>
   service { 'node-app':
     ensure => running,
     enable => true,
