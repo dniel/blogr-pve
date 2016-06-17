d9f2f8ce9461503e433732e40a474e4dedf67a4c
Daniel <daniel@engfeldt.net>
2016-06-17 17:41:56 +0200

    refactor create blogr database

--- manifests/profiles/blogr/database.pp
+++ manifests/profiles/blogr/database.pp
@@ -1,7 +1,42 @@
 class pve::profiles::blogr::database{
+  require postgresql::server
+  $db = hiera_hash('pve::profiles::database')
+
   vcsrepo { '/opt/blogr':
     ensure   => latest,
     provider => git,
     source   => 'https://github.com/dniel/blogr-workshop'
   }
+
+  postgresql::server::db { "${db['name']}":
+    user     => "${db['user']}",
+    password => postgresql_password("${db['user']}", "${db['password']}"),
+  }
+
+  postgresql::server::role { "${db['user']}":
+    password_hash => postgresql_password("${db['user']}", "${db['password']}"),
+  }
+
+  postgresql::server::database_grant { "GRANT ALL ${db['user']} - ${db['name']}:":
+    privilege   => "ALL",
+    db          => "${db['name']}",
+    role        => "${db['user']}",
+  }
+
+  postgresql_psql { "create posts table ":
+    db          => "${db['name']}",
+    command     => "create table IF NOT EXISTS posts (
+                      id serial primary key,
+                      title varchar(50) not null default '',
+                      owner varchar(30) not null default '',
+                      body varchar(1000) not null default '',
+                      created timestamptz not null default now(),
+                      updated timestamptz not null default now());",
+    unless      => "SELECT table_schema||'.'||table_name AS full_rel_name
+                    FROM information_schema.tables
+                    WHERE table_schema = 'public' and table_name = 'posts'",
+
+    require     => Postgresql::Server::Db["${db['name']}"],
+  }
+
 }
--- manifests/profiles/database/primary.pp
+++ manifests/profiles/database/primary.pp
@@ -30,7 +30,7 @@ class pve::profiles::database::primary{
     connection_limit => 5,
     password_hash    => postgresql_password("repuser", "${db['password']}"),
   }
-  
+
   postgresql::server::pg_hba_rule { "allow repuser from db-2 to replicate the database":
     description => "Open up replication for access from network",
     type        => 'host',
@@ -41,46 +41,13 @@ class pve::profiles::database::primary{
   }
   ## end.
 
-  postgresql::server::db { "${db['name']}":
-    user     => "${db['user']}",
-    password => postgresql_password("${db['user']}", "${db['password']}"),
-  }
-
-  postgresql::server::role { "${db['user']}":
-    password_hash => postgresql_password("${db['user']}", "${db['password']}"),
-  }
-
-  postgresql::server::database_grant { "GRANT ALL ${db['user']} - ${db['name']}:":
-    privilege => "ALL",
-    db        => "${db['name']}",
-    role      => "${db['user']}",
-    require     => Postgresql::Server::Db["${db['name']}"],
-  }
-
-  postgresql::server::pg_hba_rule { "allow ${db['user']} to access ${db['name']} database":
+  postgresql::server::pg_hba_rule { "allow all to access all database":
     description => "Open up PostgreSQL for access from network",
     type        => 'host',
-    database    => "${db['name']}",
-    user        => "${db['user']}",
+    database    => "all",
+    user        => "all",
     address     => '0.0.0.0/0',
     auth_method => 'md5',
-    notify      => Service['postgresqld']
-  }
-
-  postgresql_psql { "create posts table ":
-    db          => "${db['name']}",
-    command     => "create table IF NOT EXISTS posts (
-                      id serial primary key,
-                      title varchar(50) not null default '',
-                      owner varchar(30) not null default '',
-                      body varchar(1000) not null default '',
-                      created timestamptz not null default now(),
-                      updated timestamptz not null default now());",
-    unless      => "SELECT table_schema||'.'||table_name AS full_rel_name
-                    FROM information_schema.tables
-                    WHERE table_schema = 'public' and table_name = 'posts'",
-
-    require     => Postgresql::Server::Db["${db['name']}"],
   }
 
 }
