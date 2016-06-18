d773a7e72c90e4caca1fd9d3be919e8352a0b04a
Daniel <daniel@engfeldt.net>
2016-06-18 17:37:16 +0200

    read blogr app config and set in environment file

--- manifests/profiles/blogr/restapi.pp
+++ manifests/profiles/blogr/restapi.pp
@@ -1,4 +1,11 @@
 class pve::profiles::blogr::restapi{
+
+  $config = hiera_hash('pve::profiles::blogr')
+  $db_name = $config['db_name']
+  $db_host = $config['db_host']
+  $db_user = $config['db_user']
+  $db_password = $config['db_password']
+
   class { 'nodejs':
     version      => 'v6.2.0',
     make_install => false
@@ -16,7 +23,10 @@ class pve::profiles::blogr::restapi{
 
   }->
   file { '/etc/init.d/node-app':
-    source => 'puppet:///modules/pve/node-app'
+    source => 'puppet:///modules/pve/app/etc/init.d/node-app'
+  }->
+  file { '/etc/environment':
+    content => template('puppet:///modules/pve/app/etc/environment.erb')
   }~>
   service { 'node-app':
     ensure => running,
