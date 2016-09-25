6160ab1324d7dc2a26c895c3b930a61f561262ff
Daniel <daniel@engfeldt.net>
2016-09-25 11:14:07 +0200

    add logstash to logserver

--- manifests/profiles/logging/server.pp
+++ manifests/profiles/logging/server.pp
@@ -5,12 +5,20 @@ class pve::profiles::logging::server{
     repo_version => '2.x',
   }
 
+  # elasticsearch need it
   package { 'systemd-sysv':
     ensure => 'installed',
   }
 
+  # elasticsearch need it
   package { 'libaugeas-ruby':
     ensure => 'installed',
+  }
+
+  class { 'logstash':
+    manage_repo  => true,
+    repo_version => '1.5',
+    autoupgrade  => true,
   }
 
   include kibana4
