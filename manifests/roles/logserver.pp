24874c49b9cc624f1567d318774c340ab9822f81
Daniel <daniel@engfeldt.net>
2016-09-25 21:39:31 +0200

    remove puppet agent

--- manifests/roles/logserver.pp
+++ manifests/roles/logserver.pp
@@ -2,11 +2,4 @@ class pve::roles::logserver{
   include pve::profiles::common
   include pve::profiles::network
   include pve::profiles::logging::server
-
-  # Agent
-  class { '::puppet':
-    puppetmaster => "puppet-1.dragon.lan",
-    server => false
-  }
-
 }
