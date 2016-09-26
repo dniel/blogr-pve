2a0ca6dbb66bdb22c290014c6a36f4cb34699c19
Daniel <daniel@engfeldt.net>
2016-09-26 20:23:20 +0200

    add syslog forwarding

--- manifests/profiles/logging/forwarder.pp
+++ manifests/profiles/logging/forwarder.pp
@@ -1,3 +1,20 @@
 class pve::profiles::logging::forwarder{
 
+  class { 'filebeat':
+    outputs => {
+      'logstash'     => {
+        'hosts' => [
+          'log-1.dragon.lan:5044'
+        ]
+      },
+    },
+  }
+
+  filebeat::prospector { 'syslogs':
+    paths    => [
+      '/var/log/auth.log',
+      '/var/log/syslog',
+    ],
+    doc_type => 'syslog-beat',
+  }
 }
