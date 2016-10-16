aacd7a389c67cafce87b1eb06403e04a0df05176
Daniel <daniel@engfeldt.net>
2016-10-16 18:51:56 +0200

    add tmux package for login server.

---
+++ manifests/profiles/login/packages.pp
@@ -0,0 +1,7 @@
+class pve::profiles::login::packages{
+
+  package { 'tmux':
+    ensure => 'installed',
+  }
+
+}
--- manifests/roles/loginserver.pp
+++ manifests/roles/loginserver.pp
@@ -2,6 +2,7 @@ class pve::roles::loginserver{
   include pve::profiles::common
   include pve::profiles::common::packages
   include pve::profiles::common::users
+  include pve::profiles::login::packages
   include pve::profiles::network
   include pve::profiles::logging::forwarder
 }
