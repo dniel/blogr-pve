2c2877b98bdf4b64f967e9fba65a964316e5f43b
Daniel <daniel@engfeldt.net>
2016-10-16 20:06:37 +0200

    remove duplicate config

--- manifests/profiles/login.pp
+++ manifests/profiles/login.pp
@@ -6,7 +6,6 @@ class pve::profiles::login{
 
   class { 'fail2ban':
     package_ensure => 'latest',
-    config_file_template => "fail2ban/${::lsbdistcodename}/etc/fail2ban/jail.conf.erb",
   }
 
 }
