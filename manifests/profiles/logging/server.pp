9d462b101a06806db6ef9d5510485ec82a83af68
Daniel <daniel@engfeldt.net>
2016-10-22 22:32:33 +0200

    refactor logstash config to external template file

--- manifests/profiles/logging/server.pp
+++ manifests/profiles/logging/server.pp
@@ -25,50 +25,8 @@ class pve::profiles::logging::server{
 
 #  logstash::plugin { 'logstash-input-beats': }
 
-  $logstash_config = @(LOGSTASH_CONFIG)
-  input {
-    tcp {
-      port => 5000
-      type => syslog
-    }
-    udp {
-      port => 5000
-      type => syslog
-    }
-    beats {
-      port => 5044
-    }
-  }
-  filter {
-    if [type] == "blogr" {
-      grok {
-          match => { "message" => "%{COMBINEDAPACHELOG}" }
-      }
-    }
-    if [type] == "syslog" {
-      grok {
-        match => { "message" => "%{SYSLOGTIMESTAMP:syslog_timestamp} %{SYSLOGHOST:syslog_hostname} %{DATA:syslog_program}(?:\[%{POSINT:syslog_pid}\])?: %{GREEDYDATA:syslog_message}" }
-        add_field => [ "received_at", "%{@timestamp}" ]
-        add_field => [ "received_from", "%{host}" ]
-      }
-      date {
-        match => [ "syslog_timestamp", "MMM  d HH:mm:ss", "MMM dd HH:mm:ss" ]
-      }
-    }
-  }
-
-  output {
-    elasticsearch {
-      hosts => "localhost:9200"
-      manage_template => false
-      index => "%{[@metadata][beat]}-%{+YYYY.MM.dd}"
-      document_type => "%{[@metadata][type]}"
-    }
-  }
-  LOGSTASH_CONFIG
-
   logstash::configfile { 'beats_logstash_config':
-    content => $logstash_config,
+    content => template("pve/logstash/config.erb"),
   }
 
 }
