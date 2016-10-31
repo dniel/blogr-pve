dde51eabf3cded17e0c4213c2c6d2e008c95c51a
Daniel <daniel@engfeldt.net>
2016-10-31 21:05:52 +0100

    add jenkins user for all servers to use when runnings tasks from jenkins, for example puppet apply scheduled.

--- manifests/profiles/common/users.pp
+++ manifests/profiles/common/users.pp
@@ -1,4 +1,13 @@
 class pve::profiles::common::users{
 
+  class { 'accounts':
+    ssh_keys   => hiera_hash('accounts::ssh_keys', {}),
+    users      => hiera_hash('accounts::users', {}),
+    usergroups => hiera_hash('accounts::usergroups', {}),
+    shell      => '/bin/bash'
+  }
 
+  accounts::account { 'jenkins':
+    authorized_keys => ['jenkins'],
+  }
 }
--- manifests/roles/ciserver.pp
+++ manifests/roles/ciserver.pp
@@ -1,7 +1,6 @@
 class pve::roles::ciserver{
   include pve::profiles::common
   include pve::profiles::common::packages
-  include pve::profiles::common::users
   include pve::profiles::network
   include pve::profiles::jenkins
   include pve::profiles::logging::forwarder
