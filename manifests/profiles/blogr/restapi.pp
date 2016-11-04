3d7e0187a06c60d3a5d392375a81590f21de1eb2
Daniel <daniel@engfeldt.net>
2016-11-04 20:30:30 +0100

    require /opt/blogr directory

--- manifests/profiles/blogr/restapi.pp
+++ manifests/profiles/blogr/restapi.pp
@@ -1,5 +1,12 @@
 class pve::profiles::blogr::restapi{
 
+  file{"/opt/blogr":
+    ensure  =>  directory,
+  }
+  exec {"chown blogr":
+    require => [File['/opt/blogr'], User['jenkins']],
+    command => "/bin/chmod -R jenkins.jenkins /opt/blogr",
+  }
   class { 'nodejs':
     version      => 'v6.2.0',
     make_install => false
@@ -14,4 +21,6 @@ class pve::profiles::blogr::restapi{
     enable  => true,
     require => [File['/etc/init.d/node-app']]
   }
+
+
 }
