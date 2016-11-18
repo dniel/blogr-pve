80a01712f329b01ac169b726833cc1f3c5447c48
Daniel <daniel@engfeldt.net>
2016-11-18 20:51:27 +0100

    remove httpchk

--- manifests/profiles/reverseproxy.pp
+++ manifests/profiles/reverseproxy.pp
@@ -9,7 +9,6 @@ class pve::profiles::reverseproxy(
     ensure => purged,
   }
 
-
   include ::haproxy
   haproxy::listen { 'puppet00':
     collect_exported => false,
@@ -22,13 +21,11 @@ class pve::profiles::reverseproxy(
     server_names      => 'app-2.dragon.lan',
     ipaddresses       => '10.0.3.9',
     ports             => '3000',
-    options           => 'httpchk HEAD /api/system/ping HTTP/1.1',
   }
   haproxy::balancermember { 'app-4':
     listening_service => 'puppet00',
     server_names      => 'app-4.dragon.lan',
     ipaddresses       => '10.0.3.7',
     ports             => '3000',
-    options           => 'httpchk HEAD /api/system/ping HTTP/1.1',
   }
 }
