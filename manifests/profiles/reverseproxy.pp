d11eadfb08229beebc33a446e0dc7c966b96abe7
Daniel <daniel@engfeldt.net>
2016-11-18 20:21:58 +0100

    replace nginx with haproxy

--- manifests/profiles/reverseproxy.pp
+++ manifests/profiles/reverseproxy.pp
@@ -3,16 +3,28 @@ class pve::profiles::reverseproxy(
 ){
 
   class { '::nginx':
-    manage_repo    => true,
-    package_source => 'nginx-mainline'
+    ensure => 'absent'
   }
 
-  nginx::resource::upstream { 'backend':
-    ensure => "present",
-    members => $app_hosts
+  include ::haproxy
+  haproxy::listen { 'puppet00':
+    collect_exported => false,
+    ipaddress        => $::ipaddress,
+    ports            => '3000',
+    mode             => 'http',
   }
-
-  nginx::resource::vhost { "${hostname}":
-    proxy        => "http://backend"
+  haproxy::balancermember { 'app-2':
+    listening_service => 'puppet00',
+    server_names      => 'app-2.dragon.lan',
+    ipaddresses       => '10.0.3.9',
+    ports             => '3000',
+    options           => 'httpchk HEAD /api/system/ping HTTP/1.1',
+  }
+  haproxy::balancermember { 'app-4':
+    listening_service => 'puppet00',
+    server_names      => 'app-4.dragon.lan',
+    ipaddresses       => '10.0.3.7',
+    ports             => '3000',
+    options           => 'httpchk HEAD /api/system/ping HTTP/1.1',
   }
 }
