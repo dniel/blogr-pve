6a018b41ae878f02d2b1a0b6045bb42731349ca7
Daniel <daniel@engfeldt.net>
2016-11-18 21:18:58 +0100

    syntax

--- manifests/profiles/reverseproxy.pp
+++ manifests/profiles/reverseproxy.pp
@@ -21,11 +21,21 @@ class pve::profiles::reverseproxy(
     server_names      => 'app-2.dragon.lan',
     ipaddresses       => '10.0.3.9',
     ports             => '3000',
+    options           => {
+      'option'         => [
+        'httpchk HEAD /api/system/ping HTTP/1.1',
+      ]
+    }
   }
   haproxy::balancermember { 'app-4':
     listening_service => 'puppet00',
     server_names      => 'app-4.dragon.lan',
     ipaddresses       => '10.0.3.7',
     ports             => '3000',
+    options           => {
+      'option'         => [
+        'httpchk HEAD /api/system/ping HTTP/1.1',
+      ]
+    }
   }
 }
