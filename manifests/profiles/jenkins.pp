fb0e187c4aea12f60b71a30d2f4a5b646f7f18e0
Daniel <daniel@engfeldt.net>
2016-11-19 16:01:09 +0100

    upgrade always to latest jenkins version

--- manifests/profiles/jenkins.pp
+++ manifests/profiles/jenkins.pp
@@ -1,5 +1,7 @@
 class pve::profiles::jenkins{
-  include ::jenkins
+  class { 'jenkins':
+    version => 'latest'
+  }
 
   # install node so that we can build blogr.
   class { 'nodejs':
@@ -32,33 +34,33 @@ class pve::profiles::jenkins{
   #Pipeline v2.4 plugin and all its dependencies.
   jenkins::plugin { 'workflow-aggregator': }
   jenkins::plugin { 'pipeline-utility-steps': }
-  jenkins::plugin { 'workflow-cps':}
-  jenkins::plugin { 'workflow-support':}
-  jenkins::plugin { 'workflow-basic-steps':}
-  jenkins::plugin { 'pipeline-input-step':}
-  jenkins::plugin { 'pipeline-milestone-step':}
-  jenkins::plugin { 'pipeline-build-step':}
-  jenkins::plugin { 'pipeline-stage-view':}
-  jenkins::plugin { 'workflow-multibranch':}
-  jenkins::plugin { 'workflow-durable-task-step':}
-  jenkins::plugin { 'workflow-api':}
-  jenkins::plugin { 'pipeline-stage-step':}
-  jenkins::plugin { 'workflow-scm-step':}
-  jenkins::plugin { 'workflow-cps-global-lib':}
-  jenkins::plugin { 'workflow-step-api':}
-  jenkins::plugin { 'workflow-job':}
+  jenkins::plugin { 'workflow-cps': }
+  jenkins::plugin { 'workflow-support': }
+  jenkins::plugin { 'workflow-basic-steps': }
+  jenkins::plugin { 'pipeline-input-step': }
+  jenkins::plugin { 'pipeline-milestone-step': }
+  jenkins::plugin { 'pipeline-build-step': }
+  jenkins::plugin { 'pipeline-stage-view': }
+  jenkins::plugin { 'workflow-multibranch': }
+  jenkins::plugin { 'workflow-durable-task-step': }
+  jenkins::plugin { 'workflow-api': }
+  jenkins::plugin { 'pipeline-stage-step': }
+  jenkins::plugin { 'workflow-scm-step': }
+  jenkins::plugin { 'workflow-cps-global-lib': }
+  jenkins::plugin { 'workflow-step-api': }
+  jenkins::plugin { 'workflow-job': }
 
-  jenkins::plugin { 'cloudbees-folder':}
-  jenkins::plugin { 'git-server':}
-  jenkins::plugin { 'ace-editor':}
-  jenkins::plugin { 'jquery-detached':}
+  jenkins::plugin { 'cloudbees-folder': }
+  jenkins::plugin { 'git-server': }
+  jenkins::plugin { 'ace-editor': }
+  jenkins::plugin { 'jquery-detached': }
 
-  jenkins::plugin { 'handlebars':}
-  jenkins::plugin { 'pipeline-rest-api':}
-  jenkins::plugin { 'momentjs':}
+  jenkins::plugin { 'handlebars': }
+  jenkins::plugin { 'pipeline-rest-api': }
+  jenkins::plugin { 'momentjs': }
 
-  jenkins::plugin { 'pipeline-graph-analysis':}
-  jenkins::plugin { 'branch-api':}
-  jenkins::plugin { 'durable-task':}
+  jenkins::plugin { 'pipeline-graph-analysis': }
+  jenkins::plugin { 'branch-api': }
+  jenkins::plugin { 'durable-task': }
 
 }
