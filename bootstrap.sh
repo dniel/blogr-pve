3420ee6ac9c87a8d884e8a5ffdf98f63709daf3c
Daniel <daniel@engfeldt.net>
2016-11-26 20:44:45 +0100

    set mtu 1450 on eth0

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 set -x
 
 echo "Bootstrap preparing the container."
+ifconfig eth0 mtu 1450 # this is specific for my network, and probably not the same for you.
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install git ca-certificates
