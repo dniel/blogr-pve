8273ccb624e6d5aee7c79549a1e1b45da5f7f0a2
Daniel <daniel@engfeldt.net>
2017-03-15 21:05:43 +0100

    remove java module

--- manifests/profiles/logging/server.pp
+++ manifests/profiles/logging/server.pp
@@ -1,9 +1,4 @@
 class pve::profiles::logging::server{
-  java::oracle { 'jdk8' :
-    ensure  => 'present',
-    version => '8',
-    java_se => 'jdk',
-  }
 
   class { 'kibana':
     ensure => latest,
