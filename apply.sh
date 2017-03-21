391692111910af0857f59e36b9b47b1971a9d963
Daniel <daniel@engfeldt.net>
2017-03-21 19:16:17 +0100

    set --environment production

--- apply.sh
+++ apply.sh
@@ -33,7 +33,7 @@ environment=$(git symbolic-ref --short HEAD)
 ./bin/librarian-puppet install --path .. --quiet|| exit 1
 
 # Run Puppet
-./bin/puppet apply --log_level=warning --color=ansi --modulepath=.. manifests "$@"
+./bin/puppet apply --environment production --log_level=warning --color=ansi --modulepath=.. manifests "$@"
 
 ## Log status of the Puppet run
 if [ $? -eq 0 ]
