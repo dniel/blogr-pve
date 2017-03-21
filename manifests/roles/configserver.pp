7ffcf189910f4b5877cf4f1e3cc825c54a133d65
Daniel <daniel@engfeldt.net>
2017-03-21 23:18:55 +0100

    added missing common users

--- manifests/roles/configserver.pp
+++ manifests/roles/configserver.pp
@@ -1,6 +1,7 @@
 class pve::roles::configserver{
   include pve::profiles::common
   include pve::profiles::common::packages
+  include pve::profiles::common::users
   include pve::profiles::network
   include pve::profiles::logging::forwarder
   include pve::profiles::config::server
