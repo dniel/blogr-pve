04e0208416a711ec75426807e2bfc84afbc77bca
Daniel <daniel@engfeldt.net>
2017-03-21 19:06:36 +0100

    change datacenter to pve

--- manifests/profiles/config/agent.pp
+++ manifests/profiles/config/agent.pp
@@ -2,7 +2,7 @@ class pve::profiles::config::agent{
   class { '::consul':
     config_hash => {
       'data_dir'   => '/opt/consul',
-      'datacenter' => 'east-aws',
+      'datacenter' => 'pve',
       'log_level'  => 'INFO',
       'node_name'  => 'agent',
       'retry_join' => ['10.0.50.106'],
