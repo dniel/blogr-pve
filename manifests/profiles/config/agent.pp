07e003dd78f19ec91696037b241e5cbbf70ca6c4
Daniel Engfeldt <daniel@engfeldt.net>
2017-03-22 13:27:19 +0100

    Update agent.pp

--- manifests/profiles/config/agent.pp
+++ manifests/profiles/config/agent.pp
@@ -5,7 +5,7 @@ class pve::profiles::config::agent{
       'data_dir'   => '/opt/consul',
       'datacenter' => 'pve',
       'log_level'  => 'INFO',
-      'node_name'  => 'agent',
+      'node_name'  => "${::hostname}-agent",
       'retry_join' => ['10.0.50.106'],
     }
   }
