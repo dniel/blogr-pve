1215a76aa1c3b9dccecd41c4590503152e8b4b18
Daniel <daniel@engfeldt.net>
2017-03-22 22:26:11 +0100

    ok, remove again must have its own special config with systemd

--- manifests/roles/logserver.pp
+++ manifests/roles/logserver.pp
@@ -4,5 +4,4 @@ class pve::roles::logserver{
   include pve::profiles::common::users
   include pve::profiles::network
   include pve::profiles::logging::server
-  include pve::profiles::config::agent
 }
