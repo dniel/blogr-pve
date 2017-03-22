d74c317e970b1d4b76a38486ed1b2abcec8caf4b
Daniel Engfeldt <daniel@engfeldt.net>
2017-03-22 13:27:41 +0100

    Update server.pp

--- manifests/profiles/config/server.pp
+++ manifests/profiles/config/server.pp
@@ -7,7 +7,7 @@ class pve::profiles::config::server{
       'data_dir'         => '/opt/consul',
       'datacenter'       => 'pve',
       'log_level'        => 'INFO',
-      'node_name'        => 'server',
+      'node_name'        => "${::hostname}-server",,
       'server'           => true,
       'ui_dir'           => '/opt/consul/ui',
     }
