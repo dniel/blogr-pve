ba93fe78ca7622dd99165414f7377cfa2be9d2d9
Daniel <daniel@engfeldt.net>
2017-03-22 21:11:59 +0100

    add app server clusters for test and prod

--- manifests/profiles/config/server.pp
+++ manifests/profiles/config/server.pp
@@ -13,6 +13,9 @@ class pve::profiles::config::server{
     }
   }
 
+  ######
+  ## Load balancers
+  ## PROD
   ::consul::service { 'prod-lb-1':
     service_name => "lb",
     address => "10.0.10.100",
@@ -26,5 +29,65 @@ class pve::profiles::config::server{
     port    => 80,
     tags    => ['prod']
   }
+
+  ####
+  # TEST
+  #
+  ::consul::service { 'test-lb-2':
+    service_name => "lb",
+    address => "10.0.30.100",
+    port    => 80,
+    tags    => ['test']
+  }
+
+  ####
+  ## APP services
+  ## PROD
+  ::consul::service { 'prod-app-1':
+    service_name => "app",
+    address => "10.0.20.102",
+    port    => 3000,
+    tags    => ['prod']
+  }
+  ::consul::service { 'prod-app-2':
+    service_name => "app",
+    address => "10.0.20.100",
+    port    => 3000,
+    tags    => ['prod']
+  }
+  ::consul::service { 'prod-app-3':
+    service_name => "app",
+    address => "10.0.20.103",
+    port    => 3000,
+    tags    => ['prod']
+  }
+
+  ::consul::service { 'prod-app-4':
+    service_name => "app",
+    address => "10.0.20.104",
+    port    => 3000,
+    tags    => ['prod']
+  }
+
+  ########
+  ## TEST App serves
+  ##
+  ::consul::service { 'test-app-1':
+    service_name => "app",
+    address => "10.0.30.101",
+    port    => 3000,
+    tags    => ['test']
+  }
+
+  ::consul::service { 'test-app-2':
+    service_name => "app",
+    address => "10.0.30.102",
+    port    => 3000,
+    tags    => ['test']
+  }
+
+
+
+
 
 }
