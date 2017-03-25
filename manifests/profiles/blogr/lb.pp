9087e494d0bfd3f54100947ab1c2260d023da998
Daniel <daniel@engfeldt.net>
2017-03-25 12:45:50 +0100

    remove haproxy installations

--- manifests/profiles/blogr/lb.pp
+++ manifests/profiles/blogr/lb.pp
@@ -2,33 +2,8 @@ class pve::profiles::blogr::lb(
   $app_hosts,
   $server_names
 ){
-  include ::haproxy
-
-  haproxy::balancermember { 'haproxy':
-    listening_service => 'blogr_frontend',
-    server_names      => $server_names,
-    ipaddresses       => $app_hosts,
-    options           => 'check fall 3 rise 2',
-    ports             => '3000'
-  }
-
-  haproxy::frontend { 'blogr_frontend':
-    ipaddress => '0.0.0.0',
-    ports     => '80',
-    mode      => 'http',
-    options   => {
-      'use_backend' => 'blogr_backend'
-    }
-  }
-
-  haproxy::backend { 'blogr_backend':
-    mode    => 'http',
-    options => {
-      'option'  => [
-        'httpchk HEAD /api/system/ping HTTP/1.1',
-      ],
-      'balance' => 'roundrobin',
-    },
+  class { 'haproxy':
+    package_ensure => 'absent'
   }
 
   $tags = $::hostname ? {
