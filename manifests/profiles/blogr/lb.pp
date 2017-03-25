06a5dbb6220b18ebbf1cf790e9e58f93bcd17e5f
Daniel <daniel@engfeldt.net>
2017-03-25 12:51:10 +0100

    remove noop

--- manifests/profiles/blogr/lb.pp
+++ manifests/profiles/blogr/lb.pp
@@ -114,7 +114,6 @@ class pve::profiles::blogr::lb(
   }
 
   exec { 'setcap-traefik':
-    noop => true,
     command => '/sbin/setcap \'cap_net_bind_service=ep\' /opt/traefik/traefik_linux-amd64'
   }
 }
