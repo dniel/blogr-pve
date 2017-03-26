2d375b8abc0998d3b2f60ab86efc2640bda39715
Daniel <daniel@engfeldt.net>
2017-03-26 22:28:36 +0200

    add missing network profile

--- manifests/roles/chatserver.pp
+++ manifests/roles/chatserver.pp
@@ -2,6 +2,7 @@ class pve::roles::chatserver{
   include pve::profiles::common
   include pve::profiles::common::packages
   include pve::profiles::common::users
+  include pve::profiles::network
   include pve::profiles::logging::forwarder
   include pve::profiles::chatserver
   include pve::profiles::config::agent
