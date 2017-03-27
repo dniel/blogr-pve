096dd96a5b6ec8f11a2e4a120f85dcf93d361100
Daniel <daniel@engfeldt.net>
2017-03-27 22:36:56 +0200

    should reload service on config change for postgresql

--- manifests/profiles/database.pp
+++ manifests/profiles/database.pp
@@ -1,12 +1,13 @@
-class pve::profiles::database(){
+class pve::profiles::database () {
 
   class { 'postgresql::globals':
     manage_package_repo => true,
     version             => '9.4',
     encoding            => 'UTF-8',
     locale              => 'nb_NO.UTF-8',
-  }-> class { 'postgresql::server':
-    listen_addresses           => '*',
+  } -> class { 'postgresql::server':
+    listen_addresses => '*',
+    service_restart_on_change => true
   }
 
   postgresql::server::pg_hba_rule { "allow all to access all database":
