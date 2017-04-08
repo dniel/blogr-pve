60d0a38f20d818b2a0a5cc0836226e94e9f392b6
Daniel <daniel@engfeldt.net>
2017-04-08 11:36:30 +0200

    check for cpu load

--- manifests/profiles/config/agent.pp
+++ manifests/profiles/config/agent.pp
@@ -1,6 +1,6 @@
-class pve::profiles::config::agent{
+class pve::profiles::config::agent {
   class { '::consul':
-    init_style => 'debian',
+    init_style  => 'debian',
     config_hash => {
       'data_dir'   => '/opt/consul',
       'datacenter' => 'pve',
@@ -8,9 +8,12 @@ class pve::profiles::config::agent{
       'node_name'  => "${::hostname}-agent",
       'retry_join' => ['10.0.50.106'],
     },
-    checks  => [
+    checks      => [
       {
         script   => '/usr/lib/nagios/plugin/check_disk -w 50% -c 20%',
+        interval => '30s'
+      }, {
+        script   => '/usr/lib/nagios/plugins/check_load -r',
         interval => '30s'
       }
     ],
