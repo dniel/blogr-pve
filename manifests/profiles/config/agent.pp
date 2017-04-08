a29b5855963db1e3a95bf825d05a56f3efccf45a
Daniel <daniel@engfeldt.net>
2017-04-08 11:50:13 +0200

    refactor checks

--- manifests/profiles/config/agent.pp
+++ manifests/profiles/config/agent.pp
@@ -7,15 +7,16 @@ class pve::profiles::config::agent {
       'log_level'  => 'INFO',
       'node_name'  => "${::hostname}-agent",
       'retry_join' => ['10.0.50.106'],
-    },
-    checks      => [
-      {
-        script   => '/usr/lib/nagios/plugin/check_disk -w 50% -c 20%',
-        interval => '30s'
-      }, {
-        script   => '/usr/lib/nagios/plugins/check_load -r',
-        interval => '30s'
-      }
-    ],
+    }
+  }
+
+  ::consul::check { 'check_disk_usage':
+    script   => '/usr/lib/nagios/plugin/check_disk -w 50% -c 20%',
+    interval => '30s'
+  }
+
+  ::consul::check { 'check_cpuload':
+    script   => '/usr/lib/nagios/plugins/check_load -r',
+    interval => '30s'
   }
 }
