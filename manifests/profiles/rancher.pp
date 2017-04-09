f6c9ae5039d5a73dd074ad68b161d13a0abe8f8d
Daniel <daniel@engfeldt.net>
2017-04-09 20:50:40 +0200

    add consul service definition of rancher server

--- manifests/profiles/rancher.pp
+++ manifests/profiles/rancher.pp
@@ -3,4 +3,12 @@ class pve::profiles::rancher {
     manage_service => false
   }
   class { 'rancher::server': }
+
+  $tags = [$::environment,"traefik.tags=${::environment}"]
+  ::consul::service { "${::hostname}-rancher":
+    service_name => "rancher",
+    address      => $::ipaddress_eth0,
+    port         => 3000,
+    tags         => $tags
+  }
 }
