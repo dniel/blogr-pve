282362f45d4a3643bcec918121e0bca8df6245c3
Daniel <daniel@engfeldt.net>
2017-04-13 14:30:54 +0200

    remove arrows from class deps

--- manifests/profiles/config/agent.pp
+++ manifests/profiles/config/agent.pp
@@ -9,12 +9,12 @@ class pve::profiles::config::agent {
       'node_name'  => "${::hostname}-agent",
       'retry_join' => ['10.0.50.106'],
     }
-  } ->
+  }
 
   ::consul::check { 'check_disk_usage':
     script   => '/usr/lib/nagios/plugins/check_disk -M / -w 50% -c 20%',
     interval => '30s'
-  } ->
+  }
 
   ::consul::check { 'check_cpuload':
     script   => '/usr/lib/nagios/plugins/check_load -r -w 0.7 -c 1',
