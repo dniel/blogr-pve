10f96e42768a0e2d29c275335f587b4bdf274077
Daniel <daniel@engfeldt.net>
2017-04-14 10:30:28 +0200

    install oracle jdk 8

--- manifests/profiles/common/packages.pp
+++ manifests/profiles/common/packages.pp
@@ -1,16 +1,26 @@
-class pve::profiles::common::packages{
+class pve::profiles::common::packages {
 
   apt::source { 'webupd8team':
     location => 'http://ppa.launchpad.net/webupd8team/java/ubuntu',
-    repos    => 'trusty main',
+    repos    => 'main',
     key      => {
       'id'     => 'EEA14886',
       'server' => 'keyserver.ubuntu.com',
     },
+    notify   => Exec["apt_update"]
+  } -> Exec['sed -i \'s/jessie/trusty/g\' /etc/apt/sources.list.d/webupd8team.list']
+
+  exec {
+    'set-licence-selected':
+      command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections';
+
+    'set-licence-seen':
+      command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections';
   }
 
   package { 'oracle-java8-installer':
-    ensure => 'installed',
+    ensure  => 'installed',
+    require => [Apt::Source['webupd8team'], Exec['set-licence-selected'], Exec['set-licence-seen']],
   }
 
   package { 'oracle-java8-set-default':
