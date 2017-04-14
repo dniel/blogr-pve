cb698443ed757f9d04f4a3dd59ae4eeea08c18d4
Daniel <daniel@engfeldt.net>
2017-04-14 11:14:00 +0200

    move installation of java to jenkins profile, not every server need a java installation.

--- manifests/profiles/common/packages.pp
+++ manifests/profiles/common/packages.pp
@@ -1,34 +1,5 @@
 class pve::profiles::common::packages {
 
-  apt::source { 'webupd8team':
-    location => 'http://ppa.launchpad.net/webupd8team/java/ubuntu',
-    repos    => 'main',
-    key      => {
-      'id'     => 'EEA14886',
-      'server' => 'keyserver.ubuntu.com',
-    },
-    notify   => Exec["apt_update"]
-  }
-
-  exec {
-    'set-licence-selected':
-      command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections';
-
-    'set-licence-seen':
-      command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections';
-  }
-
-  package { 'oracle-java8-installer':
-    ensure  => 'installed',
-    require => [Apt::Source['webupd8team'], Exec['set-licence-selected'], Exec['set-licence-seen']],
-  } ->
-  package { 'oracle-java8-set-default':
-    ensure => 'installed',
-  }
-
-  package { 'openjdk-7-jdk':
-    ensure => 'purge'
-  }
 
 
   package { 'monitoring-plugins':
--- manifests/profiles/jenkins.pp
+++ manifests/profiles/jenkins.pp
@@ -11,9 +11,40 @@ class pve::profiles::jenkins {
     tags         => $tags
   }
 
+  apt::source { 'webupd8team':
+    location => 'http://ppa.launchpad.net/webupd8team/java/ubuntu',
+    repos    => 'main',
+    key      => {
+      'id'     => 'EEA14886',
+      'server' => 'keyserver.ubuntu.com',
+    },
+    notify   => Exec["apt_update"]
+  }
+
+  exec {
+    'set-licence-selected':
+      command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections';
+
+    'set-licence-seen':
+      command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections';
+  }
+
+  package { 'oracle-java8-installer':
+    ensure  => 'installed',
+    require => [Apt::Source['webupd8team'], Exec['set-licence-selected'], Exec['set-licence-seen']],
+  } ->
+  package { 'oracle-java8-set-default':
+    ensure => 'installed',
+  }
+
+  package { 'openjdk-7-jdk':
+    ensure => 'purge'
+  }
+
   class { 'jenkins':
     version => 'latest',
-    lts     => false
+    lts     => false,
+    java_install => false
   }
 
   # install node so that we can build blogr.
