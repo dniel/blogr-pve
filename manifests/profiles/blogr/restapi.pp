33a9829ac7c5f20757738105bdfd879a782bec71
Daniel <daniel@engfeldt.net>
2017-04-15 17:55:53 +0200

    remove docker from app container.

--- manifests/profiles/blogr/restapi.pp
+++ manifests/profiles/blogr/restapi.pp
@@ -1,11 +1,6 @@
 class pve::profiles::blogr::restapi{
   package { 'docker':
-    ensure => 'installed',
-  }
-
-  class { 'rancher':
-    registration_url => 'http://rancher.service.consul:8080/v1/scripts/443B929165E879B9F533:1483142400000:7C5YD5HkhiDu4foMN6V1XzFo6IY',
-    agent_address    => $::ipaddress_eth0
+    ensure => 'purged',
   }
 
   file{"/opt/blogr":
---
+++ manifests/roles/rancherhost.pp
@@ -0,0 +1,10 @@
+class pve::roles::rancherserver{
+  include pve::profiles::common
+  include pve::profiles::common::packages
+  include pve::profiles::common::users
+  include pve::profiles::network
+  include pve::profiles::logging::forwarder
+  include pve::profiles::config::agent
+  include pve::profiles::rancherhost
+
+}
