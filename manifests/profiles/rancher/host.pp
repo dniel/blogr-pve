587a313c78ef75767b9f2069285e674694c75d41
Daniel <daniel@engfeldt.net>
2017-04-15 18:00:32 +0200

    use rancher url from hiera

--- manifests/profiles/rancher/host.pp
+++ manifests/profiles/rancher/host.pp
@@ -6,8 +6,7 @@ class pve::profiles::rancher::host($rancher_url) {
   }
 
   class { 'rancher':
-    registration_url =>
-      'http://rancher.service.consul:8080/v1/scripts/443B929165E879B9F533:1483142400000:7C5YD5HkhiDu4foMN6V1XzFo6IY',
+    registration_url => $rancher_url,
     agent_address    => $::ipaddress_eth0
   }
 
