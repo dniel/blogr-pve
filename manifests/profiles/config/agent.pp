c1715023bc5432b1d3067619264d0209c3498eb1
Daniel <daniel@engfeldt.net>
2017-04-17 16:42:31 +0200

    set bind address to 127.0.0.1

--- manifests/profiles/config/agent.pp
+++ manifests/profiles/config/agent.pp
@@ -8,30 +8,31 @@ class pve::profiles::config::agent (
   }
 
   class { '::consul':
-    init_style    => $init_style,
-    extra_options => '-bind 0.0.0.0',
+    init_style  => $init_style,
     config_hash => {
-    'data_dir' => '/opt/consul',
-    'datacenter' => 'pve',
-    'log_level' => 'INFO',
-    'node_name' => "${::hostname}-agent",
-    'retry_join' => [$consul_server]
+      bind_addr    => '127.0.0.1',
+      client_addr  => '127.0.0.1',
+      'data_dir'   => '/opt/consul',
+      'datacenter' => 'pve',
+      'log_level'  => 'INFO',
+      'node_name'  => "${::hostname}-agent",
+      'retry_join' => [$consul_server]
+    }
   }
-}
 
-::consul::check { 'check_disk_usage':
-  script   => '/usr/lib/nagios/plugins/check_disk -w40% -c20% -p /',
-  interval => '30s'
-}
+  ::consul::check { 'check_disk_usage':
+    script   => '/usr/lib/nagios/plugins/check_disk -w40% -c20% -p /',
+    interval => '30s'
+  }
 
-::consul::check { 'check_cpuload':
-  script   => '/usr/lib/nagios/plugins/check_load -r -w 0.7 -c 1',
-  interval => '30s'
-}
+  ::consul::check { 'check_cpuload':
+    script   => '/usr/lib/nagios/plugins/check_load -r -w 0.7 -c 1',
+    interval => '30s'
+  }
 
-::consul::check { 'check_swap':
-  script   => '/usr/lib/nagios/plugins/check_swap -w 90 -c 50',
-  interval => '30s'
-}
+  ::consul::check { 'check_swap':
+    script   => '/usr/lib/nagios/plugins/check_swap -w 90 -c 50',
+    interval => '30s'
+  }
 
 }
