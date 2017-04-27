95ef24ee2297afbcf42f7bd36237c5f6fc041de4
Daniel <daniel@engfeldt.net>
2017-04-27 20:37:21 +0200

    delete unused code

--- manifests/profiles/blogr/website.pp
+++
@@ -1,3 +0,0 @@
-class pve::profiles::blogr::website{
-
-}
--- manifests/roles/appserver.pp
+++ manifests/roles/appserver.pp
@@ -2,7 +2,6 @@ class pve::roles::appserver{
   include pve::profiles::common
   include pve::profiles::common::packages
   include pve::profiles::common::users
-  include pve::profiles::blogr::website
   include pve::profiles::blogr::restapi
   include pve::profiles::network
   include pve::profiles::logging::forwarder
