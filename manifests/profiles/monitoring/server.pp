68910cd94db2b2055716a4900af23cdfd6b7005a
Daniel <daniel@engfeldt.net>
2017-04-29 12:56:23 +0200

    add consul config for prometheus targets

--- manifests/profiles/monitoring/server.pp
+++ manifests/profiles/monitoring/server.pp
@@ -1,3 +1,10 @@
-class pve::profiles::monitoring::server{
-  include ::prometheus
+class pve::profiles::monitoring::server {
+  class { 'prometheus':
+    scrape_configs => [{
+      job_name          => 'overwritten-default',
+      consul_sd_configs => {
+        server   => '127.0.0.1:8500'
+      }
+    }]
+  }
 }
