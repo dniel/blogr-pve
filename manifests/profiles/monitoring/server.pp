5003cf2d038e40b7826bfc15fc98b6080812750e
Daniel <daniel@engfeldt.net>
2017-04-29 13:25:25 +0200

    add grafana and promotheus datasource

--- manifests/profiles/monitoring/server.pp
+++ manifests/profiles/monitoring/server.pp
@@ -8,5 +8,12 @@ class pve::profiles::monitoring::server {
     }]
   }
 
-  class { 'grafana': }
+  class { 'grafana':
+  }
+
+  grafana_datasource { 'prometheus':
+    type              => 'prometheus',
+    url               => 'http://localhost:9090',
+    is_default        => true,
+  }
 }
