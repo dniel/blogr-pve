5dcacd17a84b24a5b5dcad5be4744f4b2c48a2eb
Daniel <daniel@engfeldt.net>
2017-04-30 12:17:29 +0200

    fix config so that it is a valid prometheus config syntax.

--- manifests/profiles/monitoring/server.pp
+++ manifests/profiles/monitoring/server.pp
@@ -1,19 +1,21 @@
 class pve::profiles::monitoring::server {
+
   class { 'prometheus':
+    init_style => $init_style,
     scrape_configs => [{
-      job_name          => 'overwritten-default',
-      consul_sd_configs => {
-        server   => '127.0.0.1:8500'
-      }
+      job_name          => "overwritten-default",
+      consul_sd_configs => [{
+        server   => "127.0.0.1:8500"
+      }]
     }]
   }
 
   class { 'grafana':
   }
 
-  grafana_datasource { 'prometheus':
-    type              => 'prometheus',
-    url               => 'http://localhost:9090',
-    is_default        => true,
-  }
+#  grafana_datasource { 'prometheus':
+#    type              => 'prometheus',
+#    url               => 'http://localhost:9090',
+#    is_default        => true
+#  }
 }
