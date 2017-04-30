e2b47a763dca2e843aa3f31c7d62201f9a38de9a
Daniel <daniel@engfeldt.net>
2017-04-30 12:19:35 +0200

    syntax

--- manifests/profiles/monitoring/server.pp
+++ manifests/profiles/monitoring/server.pp
@@ -1,11 +1,11 @@
 class pve::profiles::monitoring::server {
 
   class { 'prometheus':
-    init_style => $init_style,
+    init_style     => $init_style,
     scrape_configs => [{
       job_name          => "overwritten-default",
       consul_sd_configs => [{
-        server   => "127.0.0.1:8500"
+        server => "127.0.0.1:8500"
       }]
     }]
   }
@@ -13,9 +13,9 @@ class pve::profiles::monitoring::server {
   class { 'grafana':
   }
 
-#  grafana_datasource { 'prometheus':
-#    type              => 'prometheus',
-#    url               => 'http://localhost:9090',
-#    is_default        => true
-#  }
+  #  grafana_datasource { 'prometheus':
+  #    type              => 'prometheus',
+  #    url               => 'http://localhost:9090',
+  #    is_default        => true
+  #  }
 }
