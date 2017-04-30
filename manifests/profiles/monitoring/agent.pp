52ac601174f9d11551e11ff89ae462329ed20fb1
Daniel <daniel@engfeldt.net>
2017-04-30 22:30:29 +0200

    add consul service for prometheus node_exporter so that we can pick up node_exporter in prometheus as a metric exporter

--- manifests/profiles/monitoring/agent.pp
+++ manifests/profiles/monitoring/agent.pp
@@ -1,4 +1,4 @@
-class pve::profiles::monitoring::agent{
+class pve::profiles::monitoring::agent {
 
   $init_style = $::initsystem ? {
     /systemd/ => 'systemd',
@@ -7,6 +7,16 @@ class pve::profiles::monitoring::agent{
 
   class { 'prometheus::node_exporter':
     init_style => $init_style
+  }
+
+
+  $tags = [$::environment, "monitor"]
+
+  ::consul::service { "${::hostname}-node_exporter":
+    service_name => 'node_exporter',
+    address      => $::ipaddress_eth0,
+    port         => 9100,
+    tags         => $tags
   }
 
 }
