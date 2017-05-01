f7414e21eb12f61085f395c28e9dcca98c98223d
Daniel <daniel@engfeldt.net>
2017-05-01 18:13:35 +0200

    add proxmox key for apt

--- manifests/profiles/pve.pp
+++ manifests/profiles/pve.pp
@@ -3,7 +3,7 @@ class pve::profiles::pve {
     location => 'http://download.proxmox.com/debian',
     repos    => 'pve-no-subscription',
     key      => {
-      'id'     => 'BE25 7BAA 5D40 6D01 157D  323E C23A C7F4 9887 F95A',
+      'id'     => 'BE257BAA5D406D01157D323EC23AC7F49887F95A',
       'server' => 'download.proxmox.com',
     }
   }
