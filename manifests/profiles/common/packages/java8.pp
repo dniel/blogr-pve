10983eff51b143468ea5cf46f45dec7dba34d901
Daniel <daniel@engfeldt.net>
2017-05-01 12:45:15 +0200

    update java 8 apt source repo with jessie to trusty

--- manifests/profiles/common/packages/java8.pp
+++ manifests/profiles/common/packages/java8.pp
@@ -5,11 +5,13 @@ class pve::profiles::common::packages::java8 {
     key      => {
       'id'     => 'EEA14886',
       'server' => 'keyserver.ubuntu.com',
-    },
-    notify   => Exec["apt_update"]
-  }
+    }
+  } -> Exec["replace-jessie-with-trusty"] -> Exec["apt_update"]
 
   exec {
+    'replace-jessie-with-trusty':
+      command => '/bin/sed -i.bak -e \'s/jessie/trusty/\' /etc/apt/sources.list.d/webupd8team.list';
+
     'set-licence-selected':
       command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections';
 
