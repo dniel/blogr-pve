88c0d5baad6a57238bbd3fedaccdb674909648ce
Daniel <daniel@engfeldt.net>
2017-05-14 20:49:52 +0200

    add grafana as a service to consul so that it will be included in traefik

--- manifests/profiles/monitoring/server.pp
+++ manifests/profiles/monitoring/server.pp
@@ -5,6 +5,18 @@ class pve::profiles::monitoring::server {
     config_template => 'pve/prometheus/prometheus.yaml.erb'
   }
 
+  $tags = [$::environment,
+    "traefik.tags=${::environment}",
+    "traefik.frontend.rule=Host:dash.dragon.lan,dash",
+    "traefik.frontend.passHostHeader=true"]
+
+  ::consul::service { "${::hostname}-grafana":
+    service_name => 'grafana',
+    address      => $::ipaddress_eth0,
+    port         => 3000,
+    tags         => $tags
+  } ~> Service['consul']
+
   class { 'grafana':
     version => '4.2.0'
   }
