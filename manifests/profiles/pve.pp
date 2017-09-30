30dc2a4f7bd31bdab4631877d9c8fa961abf4d5c
Daniel <daniel@engfeldt.net>
2017-09-30 18:45:52 +0200

    comment out apt:source for proxmox, key fretch was stopping apply

--- manifests/profiles/pve.pp
+++ manifests/profiles/pve.pp
@@ -1,10 +1,9 @@
 class pve::profiles::pve {
-  apt::source { 'pve-no-subscription':
-    location => 'http://download.proxmox.com/debian',
-    repos    => 'pve-no-subscription',
-    key      => {
-      'id'     => 'BE257BAA5D406D01157D323EC23AC7F49887F95A',
-      'server' => 'download.proxmox.com',
-    }
-  }
+#  apt::source { 'pve-no-subscription':
+#    location => 'http://download.proxmox.com/debian',
+#    repos    => 'pve-no-subscription',
+#    key      => {
+#      'id'     => 'BE257BAA5D406D01157D323EC23AC7F49887F95A',
+#      'server' => 'download.proxmox.com',
+#    }
 }
