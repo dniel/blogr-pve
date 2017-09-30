2b0bc1b6894125935c5f7e9fad9e075f1253c422
Daniel <daniel@engfeldt.net>
2017-09-30 19:59:29 +0200

    add key to unifi repo

--- manifests/profiles/unifi.pp
+++ manifests/profiles/unifi.pp
@@ -1,7 +1,8 @@
 class pve::profiles::unifi {
   apt::source { 'testing-ubiquiti':
     location => 'http://www.ubnt.com/downloads/unifi/debian',
-    repos    => 'testing ubiquiti',
+    repos    => 'ubiquiti',
+    release  => 'testing',
     key      => {
       'id'     => '4A228B2D358A5094178285BE06E85760C0A52C50',
       'server' => 'keyserver.ubuntu.com',
