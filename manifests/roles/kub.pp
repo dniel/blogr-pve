a0634940eaadd1da44bd786519557d53e8beb679
Daniel <daniel@engfeldt.net>
2017-09-30 19:26:08 +0200

    create unifi and kubernetes role, delete old rancher roles/profiles not in use anymore.

--- manifests/roles/kub.pp
+++ manifests/roles/kub.pp
@@ -1,9 +1,8 @@
-class pve::roles::rancher{
+class pve::roles::kub{
   include pve::profiles::common
   include pve::profiles::common::packages
   include pve::profiles::common::users
   include pve::profiles::logging::forwarder
   include pve::profiles::config::agent
-  include pve::profiles::rancher::server
   include pve::profiles::monitoring::agent
 }
