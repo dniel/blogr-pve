9a35a3b7163912a222f9844ba208ec4348d37afd
Daniel <daniel@engfeldt.net>
2017-09-30 20:38:04 +0200

    consul set bootstrap true on server

--- manifests/profiles/config/server.pp
+++ manifests/profiles/config/server.pp
@@ -1,15 +1,15 @@
-class pve::profiles::config::server{
+class pve::profiles::config::server {
   class { '::consul':
-    init_style => 'debian',
+    init_style  => 'debian',
     config_hash => {
-      'bootstrap_expect' => 1,
-      'client_addr'      => '0.0.0.0',
-      'data_dir'         => '/opt/consul',
-      'datacenter'       => 'pve',
-      'log_level'        => 'INFO',
-      'node_name'        => $::hostname,
-      'server'           => true,
-      'ui_dir'           => '/opt/consul/ui',
+      'bootstrap'   => true,
+      'client_addr' => '0.0.0.0',
+      'data_dir'    => '/opt/consul',
+      'datacenter'  => 'pve',
+      'log_level'   => 'INFO',
+      'node_name'   => $::hostname,
+      'server'      => true,
+      'ui_dir'      => '/opt/consul/ui',
     }
   }
 }
