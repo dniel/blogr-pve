70d713fbdd6c48eeacf72a0f0d8a339ded038498
Daniel <daniel@engfeldt.net>
2017-09-30 19:23:14 +0200

    add role for unifi

---
+++ manifests/roles/unifi.pp
@@ -0,0 +1,9 @@
+class pve::roles::unifi{
+  include pve::profiles::common
+  include pve::profiles::common::packages
+  include pve::profiles::common::users
+  include pve::profiles::logging::forwarder
+  include pve::profiles::unifi
+  include pve::profiles::config::agent
+  include pve::profiles::monitoring::agent
+}
