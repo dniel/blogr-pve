f89c439d8f3b8bb64f16bb9f81b18ff3ed718eda
Daniel <daniel@engfeldt.net>
2017-10-05 22:50:01 +0200

    delete some old files

--- manifests/roles/chat.pp
+++
@@ -1,9 +0,0 @@
-class pve::roles::chat{
-  include pve::profiles::common
-  include pve::profiles::common::packages
-  include pve::profiles::common::users
-  include pve::profiles::logging::forwarder
-  include pve::profiles::chatserver
-  include pve::profiles::config::agent
-  include pve::profiles::monitoring::agent
-}
