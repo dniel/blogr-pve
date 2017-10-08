2f20f0636c8c88162d1a6da93fcc1324169f8cb6
Daniel <daniel@engfeldt.net>
2017-10-08 14:03:10 +0200

    add config from file

--- manifests/profiles/dns.pp
+++ manifests/profiles/dns.pp
@@ -1,9 +1,14 @@
 class pve::profiles::dns {
 
   include dnsmasq
-  dnsmasq::conf { 'consul':
+  dnsmasq::conf { 'consul-forwarding':
     ensure  => present,
     content => 'server=/consul/10.0.50.10#8600',
   }
 
+  dnsmasq::conf { 'consul-file':
+    prio   => 20,
+    ensure => present,
+    source => 'puppet:///files/etc/dnsmasq.d/consul-file',
+  }
 }
