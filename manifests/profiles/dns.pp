24be58e30031e136b0f08afa52d2de96a0b3b805
Daniel <daniel@engfeldt.net>
2017-10-08 14:19:35 +0200

    add static hosts to dns lookup

--- manifests/profiles/dns.pp
+++ manifests/profiles/dns.pp
@@ -1,6 +1,11 @@
 class pve::profiles::dns {
 
-  include dnsmasq
+  class { '::dnsmasq':
+    configs_hash    => {},
+    hosts_hash      => {},
+    dhcp_hosts_hash => {},
+  }
+
   dnsmasq::conf { 'consul':
     ensure  => present,
     content => 'server=/consul/10.0.50.10#8600',
