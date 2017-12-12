0159a40e8e41824a9a4abe6151262d57bb22404d
Daniel <daniel@engfeldt.net>
2017-12-12 20:21:38 +0100

    syntax format

--- manifests/profiles/common.pp
+++ manifests/profiles/common.pp
@@ -1,17 +1,17 @@
-class pve::profiles::common{
+class pve::profiles::common {
   Apt::Ppa <| |> -> Package <| |>
   Apt::Key<| |> -> Package<| |>
 
   include sudo
 
-# include unattended_upgrades
-# trouble to get this working on debian9
+  # include unattended_upgrades
+  # trouble to get this working on debian9
 
-#  class { 'apt':
-#    update => {
-#      frequency => 'daily',
-#    },
-#  }
+  #  class { 'apt':
+  #    update => {
+  #      frequency => 'daily',
+  #    },
+  #  }
 
 
   class { 'timezone':
@@ -21,27 +21,27 @@ class pve::profiles::common{
   class { 'ssh::server':
     storeconfigs_enabled => false,
     options              => {
-      'PermitRootLogin'  => 'without-password',
+      'PermitRootLogin'                 => 'without-password',
       'ChallengeResponseAuthentication' => 'no',
-      'PasswordAuthentication' => 'no',
-      'UsePAM' => 'no'
+      'PasswordAuthentication'          => 'no',
+      'UsePAM'                          => 'no'
     },
   }
 
   class { 'locales':
-    default_locale  => 'nb_NO.UTF-8',
-    locales         => ['en_US.UTF-8 UTF-8', 'nb_NO.UTF-8 UTF-8'],
+    default_locale => 'nb_NO.UTF-8',
+    locales        => ['en_US.UTF-8 UTF-8', 'nb_NO.UTF-8 UTF-8'],
   }
 
   file { "/opt/puppet/pve/apply.sh":
     mode => "744",
   }
 
-  file{"/opt/puppet/pve":
-    ensure  =>  directory,
+  file { "/opt/puppet/pve":
+    ensure => directory,
   }
 
-  exec {"chown pve":
+  exec { "chown pve":
     require => [File['/opt/puppet/pve'], User['jenkins']],
     command => "/bin/chown -R jenkins.jenkins /opt/puppet/pve",
   }
