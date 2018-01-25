ea7e35669aeea00abb35886f80b31c3df7973431
Daniel <daniel@engfeldt.net>
2018-01-25 22:42:16 +0100

    set default-jdk instead of oracle-jdk

--- manifests/profiles/common/packages/java8.pp
+++ manifests/profiles/common/packages/java8.pp
@@ -1,30 +1,6 @@
 class pve::profiles::common::packages::java8 {
-  apt::source { 'webupd8team':
-    location => 'http://ppa.launchpad.net/webupd8team/java/ubuntu',
-    repos    => 'main',
-    key      => {
-      'id'     => '7B2C3B0889BF5709A105D03AC2518248EEA14886',
-      'server' => 'keyserver.ubuntu.com',
-    }
-  } -> Exec["replace-jessie-with-trusty"] -> Exec["apt_update"]
 
-  exec {
-    'replace-jessie-with-trusty':
-      command => '/bin/sed -i.bak -e \'s/jessie/trusty/\' /etc/apt/sources.list.d/webupd8team.list';
-
-    'set-licence-selected':
-      command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections';
-
-    'set-licence-seen':
-      command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections';
-  }
-
-  package { 'oracle-java8-installer':
-    ensure  => 'installed',
-    require => [Apt::Source['webupd8team'], Exec['set-licence-selected'], Exec['set-licence-seen']],
-  } ->
-  package { 'oracle-java8-set-default':
+  package { 'default-jdk':
     ensure => 'installed',
   }
-
 }
