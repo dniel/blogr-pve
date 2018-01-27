5931ecc26bf18548baee02cafad5a984eec65071
Daniel <daniel@engfeldt.net>
2018-01-27 13:51:44 +0100

    add amdin user and password encrypted in eyaml

--- manifests/profiles/jenkins/slave.pp
+++ manifests/profiles/jenkins/slave.pp
@@ -1,12 +1,15 @@
-class pve::profiles::jenkins::slave {
+class pve::profiles::jenkins::slave(
+  $admin_password,
+  $admin_user
+) {
 
   include pve::profiles::common::packages::java
   include pve::profiles::common::packages::docker
 
   class { 'jenkins::slave':
     masterurl => 'http://p-ci-01.dniel.in:8080',
-    ui_user => 'adminuser',
-    ui_pass => 'adminpass',
+    ui_user => $admin_user,
+    ui_pass => $admin_password
   }
 
 }
