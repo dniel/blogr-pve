9126e91183660bbab33ca907169f70cf9011e36e
Daniel <daniel@engfeldt.net>
2018-01-27 14:34:59 +0100

    add jenkins user on cislave

--- manifests/roles/cislave.pp
+++ manifests/roles/cislave.pp
@@ -1,6 +1,7 @@
 class pve::roles::cislave{
   include pve::profiles::common
   include pve::profiles::common::packages
+  include pve::profiles::common::users
   include pve::profiles::jenkins::slave
   include pve::profiles::logging::forwarder
   include pve::profiles::config::agent
