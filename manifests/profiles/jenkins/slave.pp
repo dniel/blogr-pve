8682efb239e4f97c39cdb95a58331a83e68a9f7a
Daniel <daniel@engfeldt.net>
2018-01-28 15:21:07 +0100

    set version 3.8

--- manifests/profiles/jenkins/slave.pp
+++ manifests/profiles/jenkins/slave.pp
@@ -12,7 +12,7 @@ class pve::profiles::jenkins::slave (
     ui_user      => $admin_user,
     ui_pass      => $admin_password,
     install_java => false,
-    version      => '3.0'
+    version      => '3.8'
   }
 
   # should download swarm-client.jar from https://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/3.8/swarm-client-3.8.jar
